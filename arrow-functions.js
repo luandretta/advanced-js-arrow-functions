41de296e7a1a05f378d8d00c17c9934d2866650f
Lucimeri Andretta <113333171+luandretta@users.noreply.github.com>
2023-02-10 08:07:14 +0100

    Initial commit

---
+++ arrow-functions.js
@@ -0,0 +1,14 @@
+/**
+ * To run this file in Gitpod, use the 
+ * command node arrow-functions.js in the terminal
+ */
+
+// Vanilla JavaScript Function
+
+// Arrow Function With Parameters
+
+// Single Line Arrow Function With Parameters
+
+// Implicit Returns
+
+// Returning Multiple Lines
